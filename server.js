102e27f5dd68289a37b8ec397428a82a83f51dff
Anwara Favour <favourchika02@gmail.com>
2023-06-24 14:44:34 +0100

    Completed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Article = require('./models/article')
 const methodOverride = require('method-override')
 const connectDB = require('./db/connect')
+const {marked} = require('marked')
 require('dotenv').config()
 
 
@@ -12,7 +13,10 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
-
+marked.use({
+    mangle: false,
+    headerIds: false
+  });
 
 
 
